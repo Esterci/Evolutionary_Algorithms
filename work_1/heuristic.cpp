d330cfb6d9fc66006fcb48e94dfaac50e0b01312
Esterci <thiago.esterci.fernandes@gmail.com>
2026-07-17 18:13:20 -0300

    fix(run_heuristic):  ajusted the problem restrains

--- work_1/heuristic.cpp
+++ work_1/heuristic.cpp
@@ -33,10 +33,7 @@ void run_heuristic()
   /*generate a random solution for the random heuristic*/
   int i, help, object, tot_assigned = 0;
   int *r;
-  double energy_temp = 0.0;
-  double capacity_temp = 0.0;
-  int from, to, temp;
-  int charging_station;
+  int from, to;
 
   r = new int[NUM_OF_CUSTOMERS + 1];
   // set indexes of objects
@@ -45,7 +42,7 @@ void run_heuristic()
     r[i - 1] = i;
   }
   // randomly change indexes of objects
-  for (i = 0; i <= NUM_OF_CUSTOMERS; i++)
+  for (i = 0; i <= NUM_OF_CUSTOMERS; i++) // Tem como fixarmos e salvarmos a seed?
   {
     object = (int)((rand() / (RAND_MAX + 1.0)) * (double)(NUM_OF_CUSTOMERS - tot_assigned));
     help = r[i];
@@ -61,42 +58,15 @@ void run_heuristic()
   best_sol->steps++;
 
   i = 0;
+  
   while (i < NUM_OF_CUSTOMERS)
   {
     from = best_sol->tour[best_sol->steps - 1];
     to = r[i];
-    if ((capacity_temp + get_customer_demand(to)) <= MAX_CAPACITY && energy_temp + get_energy_consumption(from, to) <= BATTERY_CAPACITY)
-    {
-      capacity_temp += get_customer_demand(to);
-      energy_temp += get_energy_consumption(from, to);
-      best_sol->tour[best_sol->steps] = to;
-      best_sol->steps++;
-      i++;
-    }
-    else if ((capacity_temp + get_customer_demand(to)) > MAX_CAPACITY)
-    {
-      capacity_temp = 0.0;
-      energy_temp = 0.0;
-      best_sol->tour[best_sol->steps] = DEPOT;
-      best_sol->steps++;
-    }
-    else if (energy_temp + get_energy_consumption(from, to) > BATTERY_CAPACITY)
-    {
-      charging_station = rand() % (ACTUAL_PROBLEM_SIZE - NUM_OF_CUSTOMERS - 1) + NUM_OF_CUSTOMERS + 1;
-      if (is_charging_station(charging_station) == true)
-      {
-        energy_temp = 0.0;
-        best_sol->tour[best_sol->steps] = charging_station;
-        best_sol->steps++;
-      }
-    }
-    else
-    {
-      capacity_temp = 0.0;
-      energy_temp = 0.0;
-      best_sol->tour[best_sol->steps] = DEPOT;
-      best_sol->steps++;
-    }
+
+    best_sol->tour[best_sol->steps] = to;
+    best_sol->steps++;
+    i++;
   }
 
   // close EVRP tour to return back to the depot
